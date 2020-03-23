7f7323fb690b11b0948396143fd11158ce465d60
大槻亮太 <ootsukiryouta@MacBook-Pro-13-inch-2018.local>
2020-03-23 14:38:37 +0900

    EditorWindowのスクロール機能の実装

--- ObjectOrganizer.cs
+++ ObjectOrganizer.cs
@@ -3,10 +3,14 @@ using UnityEngine;
 using static UnityEditor.EditorGUILayout;
 using static UnityEngine.GUILayout;
 
-public class ObjectOrganazer : EditorWindow {
-    [MenuItem ("Plugins/ObjectOrganazer")]
-    public static void ShowWindow () {
-        GetWindow (typeof (ObjectOrganazer));
+public class ObjectOrganazer : EditorWindow
+{
+    Vector2 scrollPosition = new Vector2(0, 0);
+
+    [MenuItem("Plugins/ObjectOrganazer")]
+    public static void ShowWindow()
+    {
+        GetWindow(typeof(ObjectOrganazer));
     }
 
     GameObject prefab;
@@ -30,72 +34,94 @@ public class ObjectOrganazer : EditorWindow {
 
     bool relation = false;
 
-    void OnGUI () {
+
+
+
+    void OnGUI()
+    {
+        scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
+
+        Space();
+
         //生成するプレハブをセットする
-        Space ();
-        prefab = ObjectField ("Prefab", prefab, typeof (GameObject), false) as GameObject;
+        prefab = ObjectField("Prefab", prefab, typeof(GameObject), false) as GameObject;
 
         //子として生成するかどうか
-        asChild = BeginToggleGroup ("Creat as Child", asChild);
-        Parent = ObjectField ("ParentObject", Parent, typeof (GameObject), true) as GameObject;
-        EndToggleGroup ();
-        Space ();
+        asChild = BeginToggleGroup("Creat as Child", asChild);
+        Parent = ObjectField("ParentObject", Parent, typeof(GameObject), true) as GameObject;
+        EndToggleGroup();
+
+        Space();
 
         //次元数を選択する
-        selectedDimension = Popup ("Dimension", selectedDimension, Dimension);
-        Space ();
+        selectedDimension = Popup("Dimension", selectedDimension, Dimension);
+
+        Space();
 
         //開始地点、差、をセットする
-        StartPos = Vector3Field ("StartPos", StartPos);
-        OffsetVector = Vector3Field ("Offset", OffsetVector);
-        if (selectedDimension == 1) Label ("   ※Y-coordinate is always ０.", EditorStyles.miniLabel);
+        StartPos = Vector3Field("StartPos", StartPos);
+        OffsetVector = Vector3Field("Offset", OffsetVector);
+        if (selectedDimension == 1) Label("   ※Y-coordinate is always ０.", EditorStyles.miniLabel);
 
         //以下は、次元ごとに処理が違うので分岐する
-        switch (selectedDimension) {
+        switch (selectedDimension)
+        {
             //1次元の時
             case 0:
+                Space();
 
                 //生成するプレハブの数
-                Space ();
-                Quantity = IntField ("Quantity", Quantity);
-                Space ();
+                Quantity = IntField("Quantity", Quantity);
+
+                Space();
 
                 //オプションで、名前の番号の付け方を変更
-                OptionalSettings = BeginToggleGroup ("Optional Settings", OptionalSettings);
-                selectedOder = Popup ("Oder mode", selectedOder, Oder);
-                num0 = IntField ("Start number (Oder)", num0);
-                EndToggleGroup ();
+                OptionalSettings = BeginToggleGroup("Optional Settings", OptionalSettings);
+                selectedOder = Popup("Oder mode", selectedOder, Oder);
+                num0 = IntField("Start number (Oder)", num0);
+                EndToggleGroup();
+
+                Space();
 
                 //Cloneとして生成するか、Pfrefabとして生成するか
-                Space ();
-                relation = Toggle ("Keep relation", relation);
+                relation = Toggle("Keep relation", relation);
 
                 //生成ボタンが押された時の処理
-                if (Button ("Instantiate")) {
-                    if (prefab = null) {
-                        Debug.Log ("Not set a prefab. Instantiating prefab, it is required to set a prefab");
+                if (Button("Instantiate"))
+                {
+                    if (prefab = null)
+                    {
+                        Debug.LogError("Not set a prefab. Instantiating prefab, it is required to set a prefab");
                         return;
                     }
 
-                    for (int i = 0; i < Quantity; i++) {
+                    for (int i = 0; i < Quantity; i++)
+                    {
                         GameObject x;
 
                         //生成
-                        if (relation) {
-                            x = PrefabUtility.InstantiatePrefab (prefab) as GameObject;
-                        } else {
-                            x = Instantiate (prefab) as GameObject;
+                        if (relation)
+                        {
+                            x = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+                        }
+                        else
+                        {
+                            x = Instantiate(prefab) as GameObject;
                         }
 
                         //位置変更
                         x.transform.position = StartPos + (OffsetVector * i);
 
                         //名前付け
-                        if (OptionalSettings) {
-                            if (selectedOder == 0) {
-                                x.name = (num0 + i).ToString ();
-                            } else if (selectedOder == 1) {
-                                x.name = (num0 - i).ToString ();
+                        if (OptionalSettings)
+                        {
+                            if (selectedOder == 0)
+                            {
+                                x.name = (num0 + i).ToString();
+                            }
+                            else if (selectedOder == 1)
+                            {
+                                x.name = (num0 - i).ToString();
                             }
                         }
 
@@ -106,67 +132,82 @@ public class ObjectOrganazer : EditorWindow {
                 break;
 
             case 1:
+                Space();
+
                 //X、Y軸ごとの並べ方を入力する
-                Space ();
-                Label ("Arrangement", EditorStyles.boldLabel);
-                Ax = IntField ("Arrangement_x", Ax);
-                Az = IntField ("Arrangement_z", Az);
-                LabelField ("The number of Cube : ", (Ax * Az).ToString ());
+                Label("Arrangement", EditorStyles.boldLabel);
+                Ax = IntField("Arrangement_x", Ax);
+                Az = IntField("Arrangement_z", Az);
+                LabelField("The number of Cube : ", (Ax * Az).ToString());
 
                 //オプションで、名前の番号の付け方を変更
-                OptionalSettings = BeginToggleGroup ("Optional Settings", OptionalSettings);
-                selectedOder = Popup ("Oder mode", selectedOder, Oder);
-                num0 = IntField ("Start number (Oder_x)", num0);
-                num1 = IntField ("Start number (Oder_z)", num1);
-                reverse_XY = Toggle ("Reverse (X ⇄ Y)", reverse_XY);
-                EndToggleGroup ();
+                OptionalSettings = BeginToggleGroup("Optional Settings", OptionalSettings);
+                selectedOder = Popup("Oder mode", selectedOder, Oder);
+                num0 = IntField("Start number (Oder_x)", num0);
+                num1 = IntField("Start number (Oder_z)", num1);
+                reverse_XY = Toggle("Reverse (X ⇄ Y)", reverse_XY);
+                EndToggleGroup();
 
                 //関係性を維持するかどうか（Cloneか否か）
-                relation = Toggle ("Keep relation", relation);
+                relation = Toggle("Keep relation", relation);
 
                 //生成開始
-                if (Button ("Instantiate")) {
-
+                if (Button("Instantiate"))
+                {
                     //エラーチェック
-                    if (prefab == null) {
-                        Debug.Log ("Not set a prefab. Instantiating prefab, it is required to set a prefab");
+                    if (prefab == null)
+                    {
+                        Debug.Log("Not set a prefab. Instantiating prefab, it is required to set a prefab");
                         return;
                     }
-                    if (asChild && (Parent == null)) {
-                        Debug.Log ("Not set a parent. Creating as child, it is required to set a parent GameObject");
+                    if (asChild && (Parent == null))
+                    {
+                        Debug.Log("Not set a parent. Creating as child, it is required to set a parent GameObject");
                         return;
                     }
-                    //エラーチェック
 
-                    for (int i = 0; i < Ax; i++) {
-
-                        for (int k = 0; k < Az; k++) {
-
+                    for (int i = 0; i < Ax; i++)
+                    {
+                        for (int k = 0; k < Az; k++)
+                        {
                             GameObject x; //インスタンス
 
                             //生成
-                            if (relation) {
-                                x = PrefabUtility.InstantiatePrefab (prefab) as GameObject;
-                            } else {
-                                x = Instantiate (prefab) as GameObject;
+                            if (relation)
+                            {
+                                x = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+                            }
+                            else
+                            {
+                                x = Instantiate(prefab) as GameObject;
                             }
 
                             //座標指定
-                            x.transform.position = StartPos + new Vector3 (OffsetVector.x * k, 0, OffsetVector.z * i);
+                            x.transform.position = StartPos + new Vector3(OffsetVector.x * k, 0, OffsetVector.z * i);
 
                             //名前付け
-                            if (OptionalSettings) {
-                                if (reverse_XY) {
-                                    if (selectedOder == 0) {
-                                        x.name = (num1 + k).ToString () + "-" + (num0 + i).ToString ();
-                                    } else if (selectedOder == 1) {
-                                        x.name = (num1 - k).ToString () + "-" + (num0 - i).ToString ();
+                            if (OptionalSettings)
+                            {
+                                if (reverse_XY)
+                                {
+                                    if (selectedOder == 0)
+                                    {
+                                        x.name = (num1 + k).ToString() + "-" + (num0 + i).ToString();
                                     }
-                                } else {
-                                    if (selectedOder == 0) {
-                                        x.name = (num1 + i).ToString () + "-" + (num0 + k).ToString ();
-                                    } else if (selectedOder == 1) {
-                                        x.name = (num1 - i).ToString () + "-" + (num0 - k).ToString ();
+                                    else if (selectedOder == 1)
+                                    {
+                                        x.name = (num1 - k).ToString() + "-" + (num0 - i).ToString();
+                                    }
+                                }
+                                else
+                                {
+                                    if (selectedOder == 0)
+                                    {
+                                        x.name = (num1 + i).ToString() + "-" + (num0 + k).ToString();
+                                    }
+                                    else if (selectedOder == 1)
+                                    {
+                                        x.name = (num1 - i).ToString() + "-" + (num0 - k).ToString();
                                     }
                                 }
                             }
@@ -179,8 +220,9 @@ public class ObjectOrganazer : EditorWindow {
                 break;
         }
 
-        Space ();
-        if (Button ("Close Window")) Close ();
+        Space();
+        if (Button("Close Window")) Close();
 
+        EditorGUILayout.EndScrollView();
     }
 }
